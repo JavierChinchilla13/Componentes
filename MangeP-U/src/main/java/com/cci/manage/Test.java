f41b7cb2fb6d2c4906eebca58773c941e13b5a93
Jozandro23 <lizandroht2002@gmail.com>
2023-11-07 19:07:00 -0600

    Hola mundo

--- MangeP-U/src/main/java/com/cci/manage/Test.java
+++ MangeP-U/src/main/java/com/cci/manage/Test.java
@@ -2,4 +2,15 @@ package com.cci.manage;
 
 public class Test {
 
+	private String Hola;
+
+	public String getHola() {
+		return Hola;
+	}
+
+	public void setHola(String hola) {
+		Hola = hola;
+	}
+	
+	
 }
