a5b7bf8edf5c7cd4a98cfadc128ec3d3b66ac998
Jose <Jose@DESKTOP-UB9REI4.mshome.net>
2023-11-07 19:11:32 -0600

    Lo ven?

--- MangeP-U/src/main/java/com/cci/manage/Test.java
+++ MangeP-U/src/main/java/com/cci/manage/Test.java
@@ -1,5 +1,18 @@
 package com.cci.manage;
 
 public class Test {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
 
 }
