3eb162e2caeaa3ed04c5c70e93525dc1ecf638da
Jose <Jose@DESKTOP-UB9REI4.mshome.net>
2023-11-08 20:48:10 -0600

    Puleen

---
+++ MangeP-U/src/main/java/com/cci/manage/Proyecto.java
@@ -0,0 +1,53 @@
+package com.cci.manage;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import java.sql.Date;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Proyecto
+ *
+ */
+@Entity
+
+public class Proyecto implements Serializable {
+
+	   
+	@Id
+	private Integer Id;
+	private String nombreProyecto;
+	private String Descripcion;
+	private  Date FechaInicio;
+	private  Date FechaFin;
+	private static final long serialVersionUID = 1L;
+
+	public Proyecto() {
+		super();
+	} 
+	
+	public Integer getId() {
+		return this.Id;
+	}
+
+	public void setId(Integer Id) {
+		this.Id = Id;
+	}   
+	public String getNombreProyecto() {
+		return this.nombreProyecto;
+	}
+
+	public void setNombreProyecto(String nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}   
+	public String getDescripcion() {
+		return this.Descripcion;
+	}
+
+	public void setDescripcion(String Descripcion) {
+		this.Descripcion = Descripcion;
+	}
+   
+}
