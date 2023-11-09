2d30a0f6f14700ae3c606081c9232ba1f81e84b6
Jozandro23 <lizandroht2002@gmail.com>
2023-11-08 20:30:53 -0600

    ListoVacaciones

--- MangeP-U/src/main/java/com/cci/manage/Vacaciones.java
+++ MangeP-U/src/main/java/com/cci/manage/Vacaciones.java
@@ -3,7 +3,13 @@ package com.cci.manage;
 
 import java.io.Serializable;
 import java.lang.Integer;
+import java.sql.Date;
+
 import javax.persistence.*;
+
+import com.cci.demo.relacionUnoUno.JoinColumn;
+import com.cci.demo.relacionUnoUno.OneToOne;
+import com.cci.demo.relacionUnoUno.Persona;
 
 /**
  * Entity implementation class for Entity: Vacaciones
@@ -16,6 +22,13 @@ public class Vacaciones implements Serializable {
 	
 	private Integer idVacaciones;
 	private Integer dias;
+	private Date fech_Inicio;
+	private Date fech_Final;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id")
+	private Empleados persona;
+	private String estado;
+	
 	private static final long serialVersionUID = 1L;
 
 	public Vacaciones() {
@@ -24,16 +37,39 @@ public class Vacaciones implements Serializable {
 	public Integer getIdVacaciones() {
 		return this.idVacaciones;
 	}
-
 	public void setIdVacaciones(Integer idVacaciones) {
 		this.idVacaciones = idVacaciones;
 	}   
 	public Integer getDias() {
 		return this.dias;
 	}
-
 	public void setDias(Integer dias) {
 		this.dias = dias;
 	}
+	public Date getFech_Inicio() {
+		return fech_Inicio;
+	}
+	public void setFech_Inicio(Date fech_Inicio) {
+		this.fech_Inicio = fech_Inicio;
+	}
+	public Date getFech_Final() {
+		return fech_Final;
+	}
+	public void setFech_Final(Date fech_Final) {
+		this.fech_Final = fech_Final;
+	}
+	public Empleados getPersona() {
+		return persona;
+	}
+	public void setPersona(Empleados persona) {
+		this.persona = persona;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
    
 }
