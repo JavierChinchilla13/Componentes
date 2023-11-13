973e15e4ecfdbe07b84ccb33a44a4f9fbed258dc
Jozandro23 <lizandroht2002@gmail.com>
2023-11-12 20:32:58 -0600

    Cree el vacaciones services

---
+++ MangeP-U/src/main/java/com/cci/manage/VacacionesService.java
@@ -0,0 +1,69 @@
+package com.cci.manage;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+public class VacacionesService implements ICrud<Vacaciones>{
+
+	public VacacionesService(){
+		
+	}
+
+	@Override
+	public Vacaciones buscarPK(EntityManager em, Object obj) {
+		Vacaciones vacacionLocalizada = em.find(Vacaciones.class, obj);
+		if (vacacionLocalizada != null) {
+			return vacacionLocalizada;
+		}
+		return null;
+	}
+
+	@Override
+	public List<Vacaciones> listar(EntityManager em) {
+		
+		String jpql = "Select t From " + Empleados.class.getSimpleName() + " t";
+		List<Empleados> lista = em.createQuery(jpql, Empleados.class).getResultList();
+		
+		return null;
+	}
+
+	@Override
+	public void insertar(EntityManager em, Object obj) {
+		try {
+			em.getTransaction().begin();
+
+			em.persist(obj);
+
+			em.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void modificar(EntityManager em, Object obj) {
+
+		em.getTransaction().begin();
+
+		em.merge(obj);
+
+		em.getTransaction().commit();
+
+		
+	}
+
+	@Override
+	public void eliminar(EntityManager em, Object obj) {
+		em.getTransaction().begin();
+
+		em.remove(obj);
+
+		em.getTransaction().commit();
+		
+	}
+
+	
+}
