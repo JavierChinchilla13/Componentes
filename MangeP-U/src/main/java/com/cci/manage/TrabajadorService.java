65c58a2d096e494ab45e11a21517842936a23725
Jozandro23 <lizandroht2002@gmail.com>
2023-12-05 19:13:38 -0600

    Admin y trabajador services

---
+++ MangeP-U/src/main/java/com/cci/manage/TrabajadorService.java
@@ -0,0 +1,17 @@
+package com.cci.manage;
+
+public class TrabajadorService extends Empleados{
+
+	public TrabajadorService() {
+		super();
+	}
+	
+
+	public void IngresarTrabajador() {
+		
+		
+		
+	}
+	
+	
+}
