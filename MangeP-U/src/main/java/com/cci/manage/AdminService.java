c14154a9b92ff1445b3c133b2e4210763d5d298c
Jozandro23 <lizandroht2002@gmail.com>
2023-12-17 17:31:24 -0600

    AdminServices and trabajadorService completed

--- MangeP-U/src/main/java/com/cci/manage/AdminService.java
+++ MangeP-U/src/main/java/com/cci/manage/AdminService.java
@@ -1,5 +1,70 @@
 package com.cci.manage;
 
-public class AdminService {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+
+public class AdminService extends Empleados implements ICrud<Admin>{
+	
+	public AdminService() {
+		super();
+	}
+
+	@Override
+	public Admin buscarPK(EntityManager em, Object obj) {
+		Admin empleadoAdmin = em.find(Admin.class, obj);
+		if (empleadoAdmin != null) {
+			return empleadoAdmin;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public List<Admin> listar(EntityManager em) {
+		String jpql = "Select t From " + Admin.class.getSimpleName() + " t";
+		List<Admin> listar = em.createQuery(jpql, Admin.class).getResultList();
+		
+		return listar;
+	}
+
+	@Override
+	public void insertar(EntityManager em, Object obj) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+
+			em.persist(obj);
+
+			em.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void modificar(EntityManager em, Object obj) {
+		// TODO Auto-generated method stub
+		em.getTransaction().begin();
+
+		em.merge(obj);
+
+		em.getTransaction().commit();
+	}
+
+	@Override
+	public void eliminar(EntityManager em, Object obj) {
+		// TODO Auto-generated method stub
+		
+		em.getTransaction().begin();
+
+		em.remove(obj);
+
+		em.getTransaction().commit();
+	}
+
+	
+	
 }
