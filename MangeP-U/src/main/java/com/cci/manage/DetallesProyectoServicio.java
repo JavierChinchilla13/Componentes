d54ea6f60512ed307cd4bd66c13572604a0303da
Jose <Jose@host.docker.internal>
2023-12-18 20:12:44 -0600

    detalles

--- MangeP-U/src/main/java/com/cci/manage/DetallesProyectoServicio.java
+++ MangeP-U/src/main/java/com/cci/manage/DetallesProyectoServicio.java
@@ -3,6 +3,7 @@ package com.cci.manage;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 public class DetallesProyectoServicio implements ICrud<DetallesProyecto> {
 	
@@ -67,7 +68,33 @@ public class DetallesProyectoServicio implements ICrud<DetallesProyecto> {
 		em.getTransaction().commit();
 		
 	}
+	public void insert(EntityManager em, int empleado, int proyecto,String rol) {
+
+        try {
+            em.getTransaction().begin();
+
+            // Create a native SQL insert query
+            String nativeInsertQuery = "INSERT INTO detallesproyecto (rol, fk_proyecto_Id, fk_empleado_id) VALUES (?, ?, ?)";
+            Query query = em.createNativeQuery(nativeInsertQuery);
+
+            // Set parameters
+            query.setParameter(1, rol);
+            query.setParameter(2, proyecto);
+            query.setParameter(3, empleado);
+
+            // Execute the query
+            query.executeUpdate();
+
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        }
+
 	
 	
 
+}
 }
